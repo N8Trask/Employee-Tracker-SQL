6aebd48069129c08e0be24879855af16d4c02189
Nate Trask <trask.nate@gmail.com>
2023-06-05 22:10:39 -0500

    added comments

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const mysql = require("mysql2");
+//using mysql2, inquirer, and the formatting file
+const mysql = require("mysql2"); 
 const inquirer = require("inquirer");
 const formatRowsToTable = require("./helpers/formatting");
 
+//creating a local connection to the company_db
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -9,27 +11,29 @@ const db = mysql.createConnection({
   database: "company_db",
 });
 
+//all the selections are checked against on of these optionsActions to determine the next action
 const optionsActions = {
   "View all departments": () =>
     db
-      .promise()
-      .query(
+      .promise() //made all queries asynchronous
+      .query( //the query to return department info
         `SELECT 
                 id, 
                 name 
             FROM department;`
       )
       .then(([rows, fields]) => {
-        const table = formatRowsToTable(rows);
-        console.log(table);
-        promptOptions();
+        const table = formatRowsToTable(rows); //running the returned rows through the format function
+        console.log(table); //logging it in the terminal alredy formatted
+        promptOptions(); //trigger the inquirer prompt again
       })
-      .catch(console.log),
+      .catch(console.log), 
 
+      //similar to view all departments
   "View all roles": () =>
     db
       .promise()
-      .query(
+      .query( 
         `SELECT 
             title, 
             role.id, 
@@ -45,6 +49,7 @@ const optionsActions = {
       })
       .catch(console.log),
 
+      //similar to view all departments except this query has joins
   "View all employees": () =>
     db
       .promise()
@@ -80,6 +85,7 @@ const optionsActions = {
       ])
       .then((answer) => {
         const departmentName = answer.departmentName;
+        //passing the user input name to an INSERT statement to update the department table
         return db.promise().query("INSERT INTO department (name) VALUES (?)", departmentName, (err, result) => {
           if (err) {
             console.log(err);
@@ -111,10 +117,11 @@ const optionsActions = {
           name: "department",
           message: "Which department does the role belong to?",
           type: "list",
+          //the choices will autopopulate based on the department names in existence
           choices: () =>
             db
               .promise()
-              .query("SELECT * FROM department")
+              .query("SELECT * FROM department") //this query will return the department table for reference
               .then(([rows]) => rows.map((department) => department.name)),
         },
       ])
@@ -123,11 +130,14 @@ const optionsActions = {
         const departmentName = department;
         return db
           .promise()
+          //retrieving the department id needed to update the role table based on the department name
           .query(`SELECT id FROM department WHERE name = ?`, departmentName)
           .then(([rows]) => {
+            //mapping the id to the department_id
             const id = rows[0].id;
             return db
               .promise()
+              // inserting the role name, salary, and id to the role table
               .query("INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)", [roleName, salary, id]);
           });
       })
@@ -154,6 +164,7 @@ const optionsActions = {
           name: "role",
           message: "What is the employee's role?",
           type: "list",
+          //choices will autopopulate based on existing roles in the role table
           choices: () =>
             db
               .promise()
@@ -164,6 +175,7 @@ const optionsActions = {
           name: "manager",
           message: "Who is the emplyee's manager?",
           type: "list",
+          //choices will autopopulate from the employee table
           choices: () =>
             db
               .promise()
@@ -178,23 +190,26 @@ const optionsActions = {
         let roleID;
         return db
           .promise()
-
+          //finding the role if based on the provided role name
           .query("SELECT id FROM role WHERE title = ?", roleName)
           .then(([rows]) => {
             roleID = rows[0].id;
-
+            //splitting the user input to use in the query
             const [managerFirstName, managerLastName] = manager.split(" ");
             return db
               .promise()
+              //matching the first and last name for manager input to retrieve the employee id
               .query("SELECT id FROM employee WHERE first_name = ? AND last_name = ?", [
                 managerFirstName,
                 managerLastName,
               ]);
           })
           .then(([rows]) => {
+            //retrieving the manager id
             managerID = rows[0].id;
             return db
               .promise()
+              //passing the information need to create a new record in the employee table 
               .query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [
                 firstName,
                 lastName,
@@ -216,16 +231,19 @@ const optionsActions = {
           name: "employee",
           message: "Which employee's role do you want to update?",
           type: "list",
+          //autopopulate choices based on employees existing in the employee table
           choices: () =>
             db
               .promise()
               .query("SELECT * FROM employee")
+              //combine first and last names
               .then(([rows]) => rows.map((employee) => employee.first_name + " " + employee.last_name)),
         },
         {
           name: "roleName",
           message: "Which role do you want to assign to the selected employee?",
           type: "list",
+          //autopopulate options based on existing roles in the role table
           choices: () =>
             db
               .promise()
@@ -235,20 +253,26 @@ const optionsActions = {
       ])
       .then((answers) => {
         const { employee, roleName } = answers;
+        //creating let variables to assign the id's
         let employeeID;
         let roleID;
         return db
           .promise()
+          //query to retrieve role id based on title
           .query("SELECT id FROM role WHERE title = ?", roleName)
           .then(([rows]) => {
+            //assigning role id to roleID
             roleID = rows[0].id;
+            //splitting combined name to use in query
             const [firstName, lastName] = employee.split(" ");
             return db
               .promise()
               .query("SELECT id FROM employee WHERE first_name = ? AND last_name = ?", [firstName, lastName]);
           })
           .then(([rows]) => {
+            //assigning employee id to employeeID
             employeeID = rows[0].id;
+            //UPDATE query to change the role_id for the selected employee
             return db.promise().query("UPDATE employee SET role_id = ? WHERE id = ?", [roleID, employeeID]);
           })
           .then(([rows, fields]) => {
@@ -258,9 +282,12 @@ const optionsActions = {
           .catch(console.log);
       });
   },
+  //BONUS
   "View total expense by department": () =>
     db
       .promise()
+      //this query will sum the salaries by department. By using a right join
+      //on the employee table then we count each active employee as a record 
       .query(
         `SELECT 
             department.name,
@@ -276,10 +303,14 @@ const optionsActions = {
         promptOptions();
       })
       .catch(console.log),
+//exit option
   Exit: () => db.end(),
 };
 
+//the inquirer prompt options inside of a function
 function promptOptions() {
+
+
   inquirer
     .prompt([
       {
@@ -300,6 +331,8 @@ function promptOptions() {
       },
     ])
     .then((answer) => {
+      console.log(answer)
+        //setting the flow to the optionsActions section based on the answer
       const action = optionsActions[answer.options];
       if (action) {
         action();
@@ -307,7 +340,10 @@ function promptOptions() {
         console.log("Invalid option");
         promptOptions();
       }
-    });
+    }).catch(err => console.log(err)) 
 }
 
+
+
+//starting the application
 promptOptions();
